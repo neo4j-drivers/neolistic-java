c004228fa1ebdc2ac3223eee3f88277e8bf53580
Nigel Small <nigel.small@neotechnology.com>
2016-11-01 23:37:32 +0000

    Added transact/read/write

---
+++ src/main/java/org/neo4j/training/neolistic/Entry.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neolistic.
+ *
+ * Neolistic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.training.neolistic;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.neo4j.driver.v1.types.Node;
+
+public class Entry
+{
+    private UUID uuid;
+    private String title;
+    private long order;
+    private boolean completed;
+
+    public static Entry from( Node node )
+    {
+        Map<String, Object> properties = node.asMap();
+        return new Entry(
+                UUID.fromString( (String) properties.getOrDefault( "uuid", UUID.randomUUID().toString() ) ),
+                (String) properties.getOrDefault( "title", "" ),
+                (Long) properties.getOrDefault( "order", 0L ),
+                (Boolean) properties.getOrDefault( "completed", false ) );
+    }
+
+    private Entry( UUID uuid, String title, long order, boolean completed )
+    {
+        this.uuid = uuid;
+        this.title = title;
+        this.order = order;
+        this.completed = completed;
+    }
+
+    public UUID uuid()
+    {
+        return uuid;
+    }
+
+    public void setUUID( UUID uuid )
+    {
+        this.uuid = uuid;
+    }
+
+    public String title()
+    {
+        return title;
+    }
+
+    public void setTitle( String title )
+    {
+        this.title = title;
+    }
+
+    public long order()
+    {
+        return order;
+    }
+
+    public void setOrder( long order )
+    {
+        this.order = order;
+    }
+
+    public boolean isCompleted()
+    {
+        return completed;
+    }
+
+    public void setCompleted( boolean completed )
+    {
+        this.completed = completed;
+    }
+}
--- src/main/java/org/neo4j/training/neolistic/Neolistic.java
+++ src/main/java/org/neo4j/training/neolistic/Neolistic.java
@@ -20,50 +20,34 @@
 
 package org.neo4j.training.neolistic;
 
+import java.util.List;
+import java.util.UUID;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import org.neo4j.driver.v1.AuthToken;
 import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.exceptions.ServiceUnavailableException;
+import org.neo4j.driver.v1.types.Node;
 
-import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+
+import static spark.Spark.before;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+import static org.neo4j.driver.v1.Values.parameters;
 
 public class Neolistic
 {
-    private final Driver driver;
-
-    public Neolistic( String uri, AuthToken authToken ) throws ServiceUnavailableException
-    {
-        driver = GraphDatabase.driver( uri, authToken );
-    }
-
-    public static void main( String... args )
-    {
-        if ( args.length == 1 )
-        {
-            System.err.println( args[0] );
-            System.exit( 0 );
-        }
-        try
-        {
-            Neolistic app = new Neolistic( uri(), authToken() );
-            for ( NeolisticList list : NeolisticList.all( app.driver, 3 ) )
-            {
-                System.out.println( format( "%s [#%d]", list.title(), list.id() ) );
-                for ( int i = 1; i <= list.size(); i++ )
-                {
-                    System.out.println( format( "%d. %s", i, list.get( i - 1 ).description() ) );
-                }
-                System.out.println();
-            }
-        }
-        catch ( NeolisticException e )
-        {
-            System.err.println( e.getMessage() );
-            System.exit( 1 );
-        }
-    }
-
     private static String uri()
     {
         String uri = System.getenv( "NEO4J_URI" );
@@ -86,6 +70,46 @@ public class Neolistic
         {
             return AuthTokens.basic( user, password );
         }
+    }
+
+    private static Driver newDriver( List<String> uris, AuthToken authToken, Config config )
+    {
+        for ( String uri : uris )
+        {
+            try
+            {
+                return GraphDatabase.driver( uri, authToken, config );
+            }
+            catch ( ServiceUnavailableException ex )
+            {
+                // This URI failed, so loop around again if we have another.
+            }
+        }
+        throw new ServiceUnavailableException( "No valid database URI found" );
+    }
+
+    public static void main( String... args )
+    {
+        Driver driver = newDriver( singletonList( uri() ), authToken(), Config.defaultConfig() );
+        Gson gson = new Gson();
+        JsonParser parser = new JsonParser();
+
+        before( ( request, response ) -> response.type( "application/json" ) );
+
+        get( "/", ( request, response ) -> driver.read( tx -> tx.run( "MATCH (entry:Entry) RETURN entry" )
+                .list( record -> Entry.from( record.get( "entry" ).asNode() ) ) ), gson::toJson );
+
+        post( "/", ( request, response ) -> driver.write( tx -> {
+            JsonObject parsed = parser.parse( request.body() ).getAsJsonObject();
+            UUID uuid = UUID.randomUUID();
+            String title = parsed.get( "title" ).getAsString();
+            int order = parsed.get( "order" ).getAsInt();
+            return Entry.from( tx.run( "CREATE (entry:Entry {uuid:$uuid,title:$title,order:$order} RETURN entry",
+                    parameters( "uuid", uuid.toString(), "title", title, "order", order ) )
+                    .next().get( "entry" ).asNode() );
+        } ), gson::toJson );
+
+        enableDebugScreen();
     }
 
 }
